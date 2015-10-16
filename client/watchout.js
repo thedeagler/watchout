831ee4f7ae07fad4875cbebd3bff2adb0188f51c
thedeagler <stefan.wu@gmail.com>
2015-10-16 16:20:22 -0700

    Initialized enemy dots

--- client/watchout.js
+++ client/watchout.js
@@ -1,39 +1,49 @@
-// start slingin' some d3 here.
-// d3.select('body').append('svg');
+// Game elements
+var enemyPositions = [   ];
+var playerPosition = [  ]
 
-// Draw the mouse
-// var mouse = d3.mouse(this);
-
-
+for(var i = 0; i < 10; i++) {
+	enemyPositions.push([Math.random() * 600, Math.random() * 400]);
+}
 
 
 // add enemies
+// draw svg circle enemies on svg board
+
+d3.select('.board').selectAll('circle')
+.data(enemyPositions)
+.enter()
+.append('circle')
+.attr('r', 10)
+.attr('fill', "deeppink")
+.attr('cx', function(d) { return d[0] })
+.attr('cy', function(d) { return d[1] }); 
 
 
 
-
-
-// draw enemies in svg element
 
 
 
 // make it so enemies move to a new random location every second
 
 
+
 // make a differently colored dot to represent the player, make it draggle
-// Mouse Dragging behviour handling
+// Mouse Dragging behaviour handling
 var drag = d3.behavior.drag()
-  .on("drag", dragged)
+.on("drag", dragged)
 
 function dragged(d) {
-  d3.select(this).attr('transform', 'translate(' + d3.event.x + ',' + d3.event.y + ')');
+	playerPosition = [ d3.event.x, d3.event.y ]
+	d3.select(this).attr('transform', 'translate(' + playerPosition[0] + ',' + playerPosition[1] + ')');
 }
 
+// Draw the player dot
 d3.select('.board').append('circle')
-  .attr('transform', 'translate(' + 100 + ',' + 100 + ')')
-  .attr('r', 10)
-  .attr('fill', "black")
-  .call(drag);
+.attr('transform', 'translate(' + 100 + ',' + 100 + ')')
+.attr('r', 10)
+.attr('fill', "black")
+.call(drag);
 
 
 
