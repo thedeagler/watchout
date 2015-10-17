d1f9338ad6a88a36fcfa73696ed1afd443650e3c
thedeagler <stefan.wu@gmail.com>
2015-10-16 20:51:56 -0700

    Working on enemy collision detection when player is not dragging.

--- client/watchout.js
+++ client/watchout.js
@@ -1,6 +1,6 @@
 // Game elements
 var enemyPositions = [  ];
-var playerPosition = [  ];
+var playerPosition = [100,200];
 var ENEMY_RADIUS = 10;
 var PLAYER_RADIUS = 20;
 var ENEMY_COLOUR = "#53533B"; // I'm so fancy
@@ -9,7 +9,7 @@ var PLAYER_COLOUR = '#B6FFA6';
 // Initialize 10 enemy dots with x,y-coordinates
 generateEnemyPositions();
 function generateEnemyPositions() {
-  for(var i = 0; i < 15; i++) {
+  for(var i = 0; i < 10; i++) {
     enemyPositions[i] = [Math.random() * 600, Math.random() * 400];
   }
 }
@@ -34,13 +34,24 @@ function update() {
   var enemies = d3.select('.board').selectAll('circle')
   .data(enemyPositions)
   .transition()
+  .tween('custom', function(d) {
+
+    // can replace the custom tween function with isCollision instead
+    // OMGWTFBBQ
+    var distanceBetween = Math.sqrt(Math.pow(playerPosition[0] - d[0], 2) + Math.pow(playerPosition[1] - d[1], 2));
+    var maxDistBeforeCollision = ENEMY_RADIUS + PLAYER_RADIUS;
+
+    if(distanceBetween < maxDistBeforeCollision){
+      console.log("hit");
+    }
+  })
   .attr('cx', function(d) { return d[0] })
   .attr('cy', function(d) { return d[1] })
-  .duration(1200)
+  .duration(3000)
   .ease('bounce-in');
-}
 
-setInterval(update, 2200);
+}
+setInterval(update, 2000);
 
 
 
@@ -57,25 +68,42 @@ function dragged(d) {
 
 // Draw the player dot
 d3.select('.board').append('circle')
-.attr('transform', 'translate(' + 100 + ',' + 100 + ')')
+.attr('transform', 'translate(' + playerPosition[0] + ',' + playerPosition[1] + ')')
 .attr('r', PLAYER_RADIUS)
 .attr('fill', PLAYER_COLOUR)
 .call(drag);
 
 
-
 // detect when an enemy touches you
-var isCollision = function() {
-  enemyPositions.forEach(function(enemy) {
+function isCollision(d) {
+  var enemyCoords;
+  if (d) {
+    enemyCoords = [d];
+  } else {
+    enemyCoords = enemyPositions;
+  }
+  enemyCoords.forEach(function(enemy) {
     // for each enemy position 
     // calculate distance between the two
     // compare sum of two radii is < distance
     // return true
-    var distanceBetween = Math.sqrt( (playerPosition[0]-enemy[0])*(playerPosition[0]-enemy[0]) + (playerPosition[1]-enemy[1])*(playerPosition[1]-enemy[1]) );
+
+    var distanceBetween = Math.sqrt(Math.pow(playerPosition[0] - enemy[0], 2) + Math.pow(playerPosition[1] - enemy[1], 2));
+
     var maxDistBeforeCollision = ENEMY_RADIUS + PLAYER_RADIUS;
 
-    if (maxDistBeforeCollision > distanceBetween){
-      console.log('hit');
+    // console.log(
+    //   'distance between: ' + distanceBetween + "\n"
+    //   +' player position x: ' + playerPosition[0] + "\n"
+    //   +' enemy position x : ' + enemy[0] + "\n"
+    //   +' player position y: ' + playerPosition[1] + "\n"
+    //   +' enemy position y : ' + enemy[1] + "\n"
+    //   + ' dist between perimeters: ' + (distanceBetween - maxDistBeforeCollision) + '\n'
+    //   + "wolfram: sqrt( (" + playerPosition[0] + '-' + enemy[0] + ')^2 + (' + playerPosition[1] + '-' + enemy[1] + ')^2 )'
+    //   )
+
+    if (maxDistBeforeCollision > distanceBetween - 10){
+      console.log('hit', new Date());
       return true;
     }
     return false;
@@ -85,17 +113,3 @@ var isCollision = function() {
 
 // keep track of user's score and display it
 
-
-// use css3 animations
-
-// INFINTE LOOPS YO
-var infiniteloop = function() {
-  while(true) {
-    if(isCollision()){
-      console.log("YOU DIE");
-      break;
-    }
-  }
-}
-
-// setTimeout(infiniteloop, 10000)
