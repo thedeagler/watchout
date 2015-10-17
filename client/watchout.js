05870bb993b70128af20fcc5bb515e9ec4438b40
thedeagler <stefan.wu@gmail.com>
2015-10-16 21:59:10 -0700

    Implemented scoring table

--- client/watchout.js
+++ client/watchout.js
@@ -3,17 +3,19 @@ var enemyPositions = [  ];
 var playerPosition = [100,200];
 var ENEMY_RADIUS = 10;
 var PLAYER_RADIUS = 20;
-var ENEMY_COLOUR = "#53533B"; // I'm so fancy
-var PLAYER_COLOUR = '#B6FFA6';
+var ENEMY_COLOUR = "#DBD56E"; // I'm so fancy
+var PLAYER_COLOUR = '#66D7D1';
+var highScore = 0;
+var currentScore = 0;
+var numCollisions = 0;
 
 // Initialize 10 enemy dots with x,y-coordinates
-generateEnemyPositions();
 function generateEnemyPositions() {
   for(var i = 0; i < 10; i++) {
     enemyPositions[i] = [Math.random() * 600, Math.random() * 400];
   }
 }
-
+generateEnemyPositions();
 
 // add enemies
 // draw svg circle enemies on svg board
@@ -26,32 +28,43 @@ d3.select('.board').selectAll('circle')
 .attr('cx', function(d) { return d[0] })
 .attr('cy', function(d) { return d[1] }); 
 
-// make it so enemies move to a new random location every second
 
+
+// make it so enemies move to a new random location every second
 function update() {
   generateEnemyPositions();
 
   var enemies = d3.select('.board').selectAll('circle')
   .data(enemyPositions)
   .transition()
-  .tween('custom', function(d) {
+  // .tween('custom', function(d) {
+  //   // can replace the custom tween function with isCollision instead
+  //   // OMGWTFBBQ
+  //   var distanceBetween = Math.sqrt(Math.pow(playerPosition[0] - d[0], 2) + Math.pow(playerPosition[1] - d[1], 2));
+  //   var maxDistBeforeCollision = ENEMY_RADIUS + PLAYER_RADIUS;
+  //   if(distanceBetween < maxDistBeforeCollision){
+  //     console.log("hit");
+  //   }
+  // })
+.attr('cx', function(d) { return d[0] })
+.attr('cy', function(d) { return d[1] })
+.duration(3000)
+.ease('bounce-in');
 
-    // can replace the custom tween function with isCollision instead
-    // OMGWTFBBQ
-    var distanceBetween = Math.sqrt(Math.pow(playerPosition[0] - d[0], 2) + Math.pow(playerPosition[1] - d[1], 2));
-    var maxDistBeforeCollision = ENEMY_RADIUS + PLAYER_RADIUS;
-
-    if(distanceBetween < maxDistBeforeCollision){
-      console.log("hit");
-    }
-  })
-  .attr('cx', function(d) { return d[0] })
-  .attr('cy', function(d) { return d[1] })
-  .duration(3000)
-  .ease('bounce-in');
-
+  // update current score 
+  currentScore++;
+  if (highScore < currentScore) {
+    highScore = currentScore;
+    d3.select('.highscore').select('span').text(highScore);
+  }
+  
+  d3.select('.current').select('span').text(currentScore)
 }
+
+// call update function every 2 seconds
 setInterval(update, 2000);
+
+
 
 
 
@@ -66,12 +79,17 @@ function dragged(d) {
   isCollision();
 }
 
+
+
+
 // Draw the player dot
 d3.select('.board').append('circle')
 .attr('transform', 'translate(' + playerPosition[0] + ',' + playerPosition[1] + ')')
 .attr('r', PLAYER_RADIUS)
 .attr('fill', PLAYER_COLOUR)
 .call(drag);
+
+
 
 
 // detect when an enemy touches you
@@ -102,14 +120,20 @@ function isCollision(d) {
     //   + "wolfram: sqrt( (" + playerPosition[0] + '-' + enemy[0] + ')^2 + (' + playerPosition[1] + '-' + enemy[1] + ')^2 )'
     //   )
 
-    if (maxDistBeforeCollision > distanceBetween - 10){
-      console.log('hit', new Date());
-      return true;
-    }
-    return false;
-  })
+  // if there is a collision
+  if (maxDistBeforeCollision > distanceBetween){
+    console.log('hit')
+
+    // update collisions
+    numCollisions++;
+    d3.select('.collisions').select('span').text(numCollisions);
+    d3.select('.current').select('span').text(0);
+
+    // update high score
+
+  }
+})
 }
 
-
-// keep track of user's score and display it
+// window.requestAnimationFrame(isCollision);
 
