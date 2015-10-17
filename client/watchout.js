d594c5842e562b4661e3159f4fd36677559dd89d
thedeagler <stefan.wu@gmail.com>
2015-10-17 15:20:01 -0700

    Started polishing game. Scores and text elements update correctly, and new enemies are added each level Title and game over screens fire correctly

--- client/watchout.js
+++ client/watchout.js
@@ -1,40 +1,176 @@
 // Game elements
-var enemyPositions = [  ];
-var playerPosition = [100,200];
-var ENEMY_RADIUS = 10;
-var PLAYER_RADIUS = 20;
-var ENEMY_COLOUR = "#DBD56E"; // I'm so fancy
-var PLAYER_COLOUR = '#66D7D1';
+var enemyPositions = [];
+var playerPosition = [window.innerWidth * 0.5, window.innerHeight * 0.7];
+var ENEMY_RADIUS = 20;
+var PLAYER_RADIUS = 30;
+var ENEMY_COLOUR = '#CCDBDC'; // I'm so fancy
+var PLAYER_COLOUR = '#63C7B2';
 var highScore = 0;
 var currentScore = 0;
-// var numCollisions = 0;
-var numEnemies = 1;
+var numCollisions = 0;
+var numEnemies = 0;
+var on = 0;
+var title = d3.select('.title');
 
-// Initialize 10 enemy dots with x,y-coordinates
-function generateEnemyPositions() {
-  for(var i = 0; i < numEnemies; i++) {
-    enemyPositions[i] = [Math.random() * 600, Math.random() * 400];
-  }
-  numEnemies++;
+
+/***********************************
+
+    Input Handling
+
+***********************************/
+
+// make a differently colored dot to represent the player, make it draggable
+// Mouse Dragging behaviour handling
+var drag = d3.behavior.drag()
+  .on('drag', dragged);
+
+function dragged(d) {
+  playerPosition = [d3.event.x, d3.event.y];
+  d3.select(this).attr('transform', 'translate(' + playerPosition[0] + ',' + playerPosition[1] + ')');
+  isCollision([d]);
 }
-generateEnemyPositions();
 
-// add enemies
-// draw svg circle enemies on svg board
-d3.select('.board').selectAll('circle')
-.data(enemyPositions)
-.enter()
-.append('circle')
-.attr('class', 'enemy')
-.attr('r', ENEMY_RADIUS)
-.attr('fill', ENEMY_COLOUR)
-.attr('cx', function(d) { return d[0] })
-.attr('cy', function(d) { return d[1] })
+/***********************************
+
+    Game board Manipulation
+
+***********************************/
+
+// Draw player restriction
+d3.select('.board').append('rectangle').attr('class', 'playerBound')
+  .
+
+// Init the board with titles and players
+function initialize() {
+  title.attr('class', '');
+  // Present Title
+  title.classed('zoomInUp animated', true);
+  setTimeout(function() {
+    title.classed('zoomInUp animated', false);
+    title.classed('zoomOutUp animated', true);
+  }, 2000);
+
+  // Draw the player dot
+  d3.select('.board').append('circle').classed('player', true)
+    .attr('transform', 'translate(' + playerPosition[0] + ',' + playerPosition[1] + ')')
+    .attr('r', PLAYER_RADIUS)
+    .attr('fill', PLAYER_COLOUR)
+    .call(drag);
+
+  // Generate enemy positions
+  generateEnemyPositions();
+
+  // add first enemy
+  // draw svg circle enemies on svg board
+  d3.select('.board').selectAll('circle')
+    .data(enemyPositions)
+    .enter()
+    .append('circle')
+    .attr('class', 'enemy')
+    .attr('r', ENEMY_RADIUS)
+    .attr('fill', ENEMY_COLOUR)
+    .attr('cx', function(d) {
+      return d[0];
+    })
+    .attr('cy', function(d) {
+      return d[1];
+    });
+}
+
+// Update the board
+//  Add new enemies, and move old ones
+function update() {
+  d3.select('.board').selectAll('.enemy')
+    .data(enemyPositions)
+    .enter()
+    .append('circle')
+    .attr('class', 'enemy')
+    .attr('r', ENEMY_RADIUS)
+    .classed('flash animated', true)
+    .attr('fill', ENEMY_COLOUR)
+    .attr('cx', function(d) {
+      return d[0];
+    })
+    .attr('cy', function(d) {
+      return d[1];
+    });
+
+  // Update enemy positions
+  d3.select('.board').selectAll('.enemy')
+    .data(enemyPositions)
+    .transition()
+    .tween('custom', tweenWithCollisionDetection)
+    .duration(2000)
+    .ease('bounce-out');
+
+  // Calculate new enemy positions
+  generateEnemyPositions();
+  numEnemies++;
+
+  // update current score
+  currentScore = numEnemies - 2 > 0 ? numEnemies - 2 : 0
+  if (highScore < currentScore) {
+    highScore = currentScore;
+    d3.select('.highscore').select('span').text(highScore);
+  }
+
+  d3.select('.current').select('span').text(currentScore);
+}
+
+// Generates next enemy position
+function generateEnemyPositions() {
+  for (var i = 0; i < numEnemies; i++) {
+    enemyPositions[i] = [Math.random() * window.innerWidth, Math.random() * window.innerHeight];
+  }
+}
 
 
+/***********************************
 
-// Tween Collision
-tweenWithCollisionDetection = function(endData) {
+    Event Handlers
+
+***********************************/
+
+// detect when an enemy touches you
+function isCollision(d) {
+  var distanceBetween = Math.sqrt(Math.pow(playerPosition[0] - d[0], 2) + Math.pow(playerPosition[1] - d[1], 2));
+  var maxDistBeforeCollision = ENEMY_RADIUS + PLAYER_RADIUS;
+
+  if (maxDistBeforeCollision > distanceBetween) {
+    collisionHandler();
+    numCollisions++;
+    d3.select('.collisions').select('span').text(numCollisions);
+  }
+}
+
+// Game end handler
+function collisionHandler() {
+  numEnemies = 0;
+  enemyPositions = [];
+  currentScore = 0;
+
+  generateEnemyPositions();
+
+  // Remove enemies
+  d3.select('.board').selectAll('.enemy')
+    .data(enemyPositions)
+    .exit()
+    .remove();
+
+  // Set text elements
+  d3.select('.current').select('span').text(0);
+
+  d3.select('.over').attr('class', '').attr('class', 'over zoomInUp animated');
+  setTimeout(function() {
+    d3.select('.over').attr('class', '').attr('class', 'over zoomOutUp animated');
+  }, 1000)
+
+  // Reinit gameboard
+  setTimeout(initialize, 2000);
+}
+
+// Tween Collision Calculations
+function tweenWithCollisionDetection(endData) {
   var endPos, enemy, startPos;
   enemy = d3.select(this);
   startPos = {
@@ -45,133 +181,25 @@ tweenWithCollisionDetection = function(endData) {
     x: endData[0],
     y: endData[1]
   };
-  // console.log(startPos.x + ' : ' + endData[0]);
+
   return function(t) {
     var enemyNextPos;
     enemyNextPos = {
       x: startPos.x + (endPos.x - startPos.x) * t,
       y: startPos.y + (endPos.y - startPos.y) * t
     };
-    var wah = [enemyNextPos.x, enemyNextPos.y]
-    isCollision(wah);
+    var enemyArr = [enemyNextPos.x, enemyNextPos.y];
+    isCollision(enemyArr);
     return enemy.attr('cx', parseFloat(enemyNextPos.x)).attr('cy', parseFloat(enemyNextPos.y));
   };
 };
 
+/***********************************
 
-// make it so enemies move to a new random location every second
-function update() {
-  d3.select('.board').selectAll('circle')
-  .data(enemyPositions)
-  .enter()
-  .append('circle')
-  .attr('class', 'enemy')
-  .attr('r', ENEMY_RADIUS)
-  .attr('fill', ENEMY_COLOUR)
-  .attr('cx', function(d) { return d[0] })
-  .attr('cy', function(d) { return d[1] })
+    GAME STARTS HERE!
 
-  // Update enemy positions
-  var enemies = d3.select('.board').selectAll('circle')
-  .data(enemyPositions)
-  .transition()
-  .tween('custom', tweenWithCollisionDetection)
-  // .attr('cx', function(d) { return d[0] })
-  // .attr('cy', function(d) { return d[1] })
-  .duration(2000)
-  .ease('bounce-in');
-
-  // Calculate ne enemy positions
-  generateEnemyPositions();
-
-  // update current score 
-  currentScore++;
-  if (highScore < currentScore) {
-    highScore = currentScore;
-    d3.select('.highscore').select('span').text(highScore);
-  }
-
-  d3.select('.current').select('span').text(currentScore)
-}
+***********************************/
+initialize();
 
 // call update function every 2 seconds
 setInterval(update, 2000);
-
-
-
-
-
-// make a differently colored dot to represent the player, make it draggle
-// Mouse Dragging behaviour handling
-var drag = d3.behavior.drag()
-.on("drag", dragged)
-
-function dragged(d) {
-  playerPosition = [ d3.event.x, d3.event.y ]
-  d3.select(this).attr('transform', 'translate(' + playerPosition[0] + ',' + playerPosition[1] + ')');
-  isCollision([d]);
-}
-
-
-
-
-// Draw the player dot
-d3.select('.board').append('circle')
-.attr('transform', 'translate(' + playerPosition[0] + ',' + playerPosition[1] + ')')
-.attr('r', PLAYER_RADIUS)
-.attr('fill', PLAYER_COLOUR)
-.call(drag);
-
-
-
-
-// detect when an enemy touches you
-function isCollision(d) {
-  // var enemyCoords;
-  // if (d) {
-  //   enemyCoords = [d];
-  // } else {
-  //   enemyCoords = enemyPositions;
-  // }
-  // enemyCoords.forEach(function(enemy) {
-    // for each enemy position 
-    // calculate distance between the two
-    // compare sum of two radii is < distance
-    // return true
-
-    var distanceBetween = Math.sqrt(Math.pow(playerPosition[0] - d[0], 2) + Math.pow(playerPosition[1] - d[1], 2));
-
-    var maxDistBeforeCollision = ENEMY_RADIUS + PLAYER_RADIUS;
-
-    // console.log(
-    //   'distance between: ' + distanceBetween + "\n"
-    //   +' player position x: ' + playerPosition[0] + "\n"
-    //   +' enemy position x : ' + enemy[0] + "\n"
-    //   +' player position y: ' + playerPosition[1] + "\n"
-    //   +' enemy position y : ' + enemy[1] + "\n"
-    //   + ' dist between perimeters: ' + (distanceBetween - maxDistBeforeCollision) + '\n'
-    //   + "wolfram: sqrt( (" + playerPosition[0] + '-' + enemy[0] + ')^2 + (' + playerPosition[1] + '-' + enemy[1] + ')^2 )'
-    //   )
-    if (maxDistBeforeCollision > distanceBetween) {
-      collisionHandler();
-    }
-  }
-
-
-function collisionHandler() {
-  console.log('hit')
-  // End game
-  numEnemies = 1;
-  enemyPositions = [];
-  generateEnemyPositions();
-  // debugger;
-
-  d3.select('.board').selectAll('.enemy')
-  .data(enemyPositions)
-  .exit()
-  .remove();
-
-  currentScore = 0;
-  d3.select('.current').select('span').text(0);
-}
-
